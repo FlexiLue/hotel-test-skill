8e276f599556fe7a41b6517056b639569eb83fd4
Felix <felix.luette@web.de>
2023-06-12 16:32:04 +0200

    changed folder structure and changed intent parser to adapt-parser

--- __init__.py
+++ __init__.py
@@ -1,13 +1,18 @@
-from mycroft import MycroftSkill, intent_file_handler
+from mycroft import MycroftSkill, intent_file_handler, intent_handler
+from adapt.intent import IntentBuilder
 
 
 class HotelTest(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('test.hotel.intent')
-    def handle_test_hotel(self, message):
-        self.speak_dialog('test.hotel')
+    # @intent_file_handler('test.hotel.intent')
+    # def handle_test_hotel(self, message):
+    #     self.speak_dialog('test.hotel')
+
+    @intent_handler(IntentBuilder('BreakfastTimeIntent').require('BreakfastTimesKeyword'))
+    def handle_breakfast_times_intent(self, message):
+        self.speak_dialog('test.hotel.dialog')
 
 
 def create_skill():
