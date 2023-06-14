a98b01d571c2cefe18979ceffad7a89f0abf6889
Felix <felix.luette@web.de>
2023-06-14 17:59:13 +0200

    Schaden log added

--- __init__.py
+++ __init__.py
@@ -18,8 +18,8 @@ class HotelTest(MycroftSkill):
     #Spiegellampe Kaputt Prozess
     @intent_handler(IntentBuilder("DamageReportingIntent").require("DamageReportingKeyword").optionally("location"))
     def handle_damage_reporting_intent(self, message):
-        self.log.info(message.data)
         schaden = self.get_response('DamageReportingFirstQuestion')
+        self.log.info(schaden)
         self.speak_dialog('Vielen Dank, dass du uns dein Problem meldest.')
 
     
